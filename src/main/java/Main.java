1855d52c6779294f74d71289a12f47af90050e38
Andrew Schwartz <andrew@aschwartz.me>
2017-04-07 18:13:24 -0600

    add total to player class, updat Main accordingly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,54 +12,49 @@ public class Main {
         players.getUser().getCards().add(deck.draw());
         players.getUser().getCards().add(deck.draw());
 
-        System.out.println("You draw a " + players.getUser().getCards().get(0) + " and a " + players.getUser().getCards().get(1) + "." +
-                "\nThe dealer has a " + players.getComp().getCards().get(0) + " face up.");
-        int userTotal = 0;
-        int dealerTotal = 0;
-        for (Card c : players.getUser().getCards()) {userTotal += c.value;}
-        System.out.print("Your hand: ");
-        printHand(players.getUser(), userTotal);
-        while (userTotal <= 21) {
+        System.out.println("The dealer has a " + players.getComp().getCards().get(0) + " face up.");
+        System.out.print("Your hand: "); printHand(players.getUser(), players.getUser().getTotal());
+        Scanner s = new Scanner(System.in);
+        while (players.getUser().getTotal() <= 21) {
             System.out.println("Would you like to hit (h) or stick (s)?");
-            Scanner s = new Scanner(System.in);
             String selection = s.nextLine();
             if (selection.equals("h")) {
                 Card d = deck.draw();
                 players.getUser().getCards().add(d);
-                userTotal += d.value;
+                players.getUser().setTotal(players.getUser().getTotal() + d.value);
                 System.out.print("Your hand:");
-                printHand(players.getUser(), userTotal);
+                printHand(players.getUser(), players.getUser().getTotal());
             } else if (selection.equals("s")) {
                 break;
-            } else {}
-        }
-
-        for (Card c : players.getComp().getCards()) {dealerTotal += c.value;}
-        if (userTotal <= 21 && userTotal < 21) {
-            System.out.println("The dealer has: " + dealerTotal);
-            printHand(players.getComp(), dealerTotal);
-            while (dealerTotal <= 17) {
-                Card d = deck.draw();
-                players.getComp().getCards().add(d);
-                dealerTotal += d.value;
-                System.out.println("The dealer now has: ");
-                printHand(players.getComp(), dealerTotal);
             }
         }
-        if (dealerTotal > 21) {
+        s.close();
+
+        if (players.getUser().getTotal() <= 21 && players.getComp().getTotal() < 21) {
+            System.out.println("The dealer has: " + players.getComp().getTotal());
+            printHand(players.getComp(), players.getComp().getTotal());
+            while (players.getComp().getTotal() <= 17) {
+                Card d = deck.draw();
+                players.getComp().getCards().add(d);
+                players.getComp().setTotal(players.getComp().getTotal() + d.value);
+                System.out.println("The dealer now has: ");
+                printHand(players.getComp(), players.getComp().getTotal());
+            }
+        }
+        if (players.getComp().getTotal() > 21) {
             System.out.println("Dealer Busted!");
-            System.out.println("You Win! Dealer busted at " + dealerTotal + ", you had " + userTotal + ".");
-        } else if (userTotal > 21) {
-            System.out.println("You busted at " + userTotal + "!");
-        } else if (dealerTotal > userTotal) {
-            System.out.println("Dealer Wins! Dealer had " + dealerTotal + ", you had " + userTotal + ".");
-        } else if (userTotal > dealerTotal) {
-            System.out.println("You Win! Dealer had " + dealerTotal + ", you had " + userTotal + ".");
-        } else if (userTotal == dealerTotal) {
-            System.out.println("Push (aka tie). Dealer had " + dealerTotal + ", you had " + userTotal + ".");
+            System.out.println("You Win! Dealer busted at " + players.getComp().getTotal() + ", you had " + players.getUser().getTotal() + ".");
+        } else if (players.getUser().getTotal() > 21) {
+            System.out.println("You busted at " + players.getUser().getTotal() + "!");
+        } else if (players.getComp().getTotal() > players.getUser().getTotal()) {
+            System.out.println("Dealer Wins! Dealer had " + players.getComp().getTotal() + ", you had " + players.getUser().getTotal() + ".");
+        } else if (players.getUser().getTotal() > players.getComp().getTotal()) {
+            System.out.println("You Win! Dealer had " + players.getComp().getTotal() + ", you had " + players.getUser().getTotal() + ".");
+        } else if (players.getUser().getTotal() == players.getComp().getTotal()) {
+            System.out.println("Push (aka tie). Dealer had " + players.getComp().getTotal() + ", you had " + players.getUser().getTotal() + ".");
         }
     }
-    public static void printHand(Player p, int total) {
+    static void printHand(Player p, int total) {
         for (Card c : p.cards) {System.out.print(c + " ");}
         System.out.println("(" + total + ")");
     }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,10 +3,25 @@ import java.util.ArrayList;
 /**
  * Created by werdn on 4/7/17.
  */
-public class Player {
+class Player {
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
     ArrayList<Card> cards = new ArrayList<Card>();
+    int Total;
+
+    public void setTotal(int total) {
+        Total = total;
+    }
+
+    public int getTotal() {
+        this.Total = 0;
+        for (Card c : this.getCards()) {this.Total += c.value;}
+        return Total;
+    }
 
     public Player() {
-
+        Total = 0;
     }
 }
