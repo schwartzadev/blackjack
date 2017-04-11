9a0679640106341f6e2bd5af827806c565d6d08e
Andrew Schwartz <andrew@aschwartz.me>
2017-04-10 18:08:21 -0600

    add bet instance

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 /**
  * Created by werdn on 4/7/17.
  */
-public class Main {
-    public static void main(String[] args) {
-        while (true) {
-            Game.Play(); }
+    public class Main {
+        public static Bet bet = new Bet();
+        public static void main(String[] args) {
+            while (true) {
+                Game.Play(); }
+        }
     }
-
-}
